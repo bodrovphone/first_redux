7b76f8765d365306d6da6de9803971fd152fb821
Oleksandr Bodrov <bodrovphone@gmail.com>
2017-04-03 17:19:08 +0300

    '0.0.1'

--- src/index.js
+++ src/index.js
@@ -9,21 +9,22 @@
 
 import { createStore } from 'redux';
 
-function playlist(state = [], action) {
-  if (action.type === 'ADD_TRACK') {
-    return [
-      ...state,
-      action.payload
-    ];
-  }
-  return state;
-}
-
 const store = createStore(playlist);
 
-store.subscribe(() => {
-  console.log('subscribe', store.getState());
-})
+function playlist(state = [], action) {
+    if (action.type === 'ADD_TRACK') {
+        return [
+            ...state,
+            action.payload
+        ];
+    }
+    return state;
 
-store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
-store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
+}
+
+store.subscribe(() => {
+    console.log('subscribe', store.getState());
+});
+
+store.dispatch({type: 'ADD_TRACK', payload: 'Smells like spirit'});
+store.dispatch({type: 'ADD_TRACK', payload: 'Vopli vid Oplyasova'});
